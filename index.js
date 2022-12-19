2142efb46969f6775ef84927dce46ea0109b5212
Thomas <tfnielson.se@gmail.com>
2022-12-18 20:53:19 -0700

    commit tree 9dfd524aee985e61cab4404a457f84c36f496503

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
+const totalBatteries = batteryBatches.reduce(function(sum, batches) {
+    return batches + sum;
+})
+
 // Code your solution here
+console.log(totalBatteries);
